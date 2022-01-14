b2db028a8f64a0a3628e453cd75f4062ac910e8e
Robert Barnes (DevOps Rob) <rbarnes@hashicorp.com>
2022-01-14 16:51:45 +0000

    Added license and output description

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 output "azure_groups_map" {
-  value     = local.azure_groups
-  sensitive = true
+  value       = local.azure_groups
+  sensitive   = true
+  description = "Map of Azure AD group names to Object IDs."
 }
